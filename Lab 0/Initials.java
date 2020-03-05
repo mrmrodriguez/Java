e91e9f777af68c686362e2a6fdfa03d526210007
Megan Rodriguez <60858719+mrmrodriguez@users.noreply.github.com>
2020-03-05 15:22:05 -0600

    Create Initials.java

---
+++ Lab 0/Initials.java
@@ -0,0 +1,18 @@
+//This program prints a header followed by three initials in block letters
+
+public class Initials {
+  public static void main(String[] args) {
+    System.out.println("Lab 0 written by Megan Rodriguez");
+    System.out.println("M         M  M        M  RRRRRR");
+    System.out.println("MM       MM  MM       M  R      R");
+    System.out.println("M M     M M  M M     MM  R       R");
+    System.out.println("M  M   M  M  M  M   M M  R       R");
+    System.out.println("M   M M   M  M   M M  M  R      R");
+    System.out.println("M    M    M  M    M   M  RRRRRR");
+    System.out.println("M         M  M        M  R     R");
+    System.out.println("M         M  M        M  R      R");
+    System.out.println("M         M  M        M  R       R");
+    System.out.println("M         M  M        M  R        R");
+    System.out.println("M         M  M        M  R         R");
+  }
+}
